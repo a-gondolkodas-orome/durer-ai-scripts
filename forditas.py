e57035de0acdcb7bbf5527a24fa8d895055bd070
alex <kempfxela@gmail.com>
2025-07-05 11:55:39 +0200

    Now we working in a subdirectory.

--- forditas.py
+++ forditas.py
@@ -4,12 +4,16 @@ import shutil
 from dotenv import load_dotenv
 
 
-FILES_DIR = "megoldasok"
-REMOVED_DIR = "megoldasok_en"
-RETURN_DIR = "megoldasok_leforditva"
+MAIN_DIR="Fő mappa"
+FILES_DIR = "hu_megoldasok"
+REMOVED_DIR = "en_megoldasok"
+RETURN_DIR = "dontoK"
 AI_PROMPT = "Egy matekverseny feladatát szeretném angolra fordítani. Bemásolok a feladatot kérlek fordítsd át, a neveknek a kézenfekvő angol verzióját add meg. Válaszként csak a fordítás szövegét add meg. Always keep latex formatting in the text, e.g. \textit{...}. You can delete the comments (lines beginning with %), but leave the figures unchanged. The problem:\n"
+AI_MODEL="anthropic/claude-sonnet-4"
 
-
+FILES_DIR=f"{MAIN_DIR}/{FILES_DIR}"
+REMOVED_DIR=f"{MAIN_DIR}/{REMOVED_DIR}"
+RETURN_DIR=f"{MAIN_DIR}/{RETURN_DIR}"
 load_dotenv()
 
 files = [
@@ -24,9 +28,8 @@ needed_files = list(set(files).difference(set(removed_files)))
 
 
 os.makedirs(RETURN_DIR, exist_ok=True)
-
 for file_name in needed_files:
-    if file_name[:-4]==".tex":
+    if file_name[-4:]==".tex":
         with open(f"{FILES_DIR}/{file_name}", "r", encoding="utf-8") as file:
             file_list = file.readlines()
             text = ""
@@ -42,7 +45,7 @@ for file_name in needed_files:
                 "HTTP-Referer": "<YOUR_SITE_URL>",  # Optional. Site URL for rankings on openrouter.ai.
                 "X-Title": "<YOUR_SITE_NAME>",  # Optional. Site title for rankings on openrouter.ai.
             },
-            model="openai/o3",
+            model=AI_MODEL,
             messages=[
                 {
                     "role": "user",
